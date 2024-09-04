7fe052b89ab00ff85266b7f1c5ca48abf8ee428f
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-04 10:55:57 +0800

    model signal custom 2 way binding

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, model, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,13 +9,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  @Input({ required: true }) size?: {width: string, height: string}; // Custom 2 way binding for size property
-  @Output() sizeChange = new EventEmitter(); // Custom 2 way binding for size property
+  // @Input({ required: true }) size?: {width: string, height: string}; // Custom 2 way binding for size property
+  // @Output() sizeChange = new EventEmitter(); // Custom 2 way binding for size property
+  
+  size = model.required<{width: string, height: string}>();
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.set({
       width: '200',
-      height: '100',
+      height: '100'
     });
   }
 }
