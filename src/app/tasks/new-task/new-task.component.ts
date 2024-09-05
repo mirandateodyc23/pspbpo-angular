93da0ffaec7d35fa3f31670aa29b000faada4f79
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-05 14:05:11 +0800

    add tasks.service.ts

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,7 +12,16 @@ import { FormsModule } from '@angular/forms';
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  // private tasksService: TasksService;
+
+  constructor(private tService: TasksService) {
+    // this.tasksService = new TasksService();
+    // this.tasksService = tService;
+  }
+
   onAddTask(title: string, description: string) {
+    // this.tasksService.addTask({title: title, description: description});
+    this.tService.addTask({title, description});
     this.formEl()?.nativeElement.reset();
   }
 }
