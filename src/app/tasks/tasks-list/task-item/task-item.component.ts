4dc3fe9fa12e16aa0ece3271eb2cf0e496e9f7e2
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-05 17:01:13 +0800

    add service into service

--- src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -49,6 +49,5 @@ export class TaskItemComponent {
     
     this.tasksService.updateTaskStatus(taskId, newStatus);
 
-    console.log(newStatus);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { TasksService } from './app/tasks/tasks.service';
+import { LoggingService } from './app/logging.service';
 
-bootstrapApplication(AppComponent
-//     , {
+// bootstrapApplication(AppComponent, {
 //     providers: [TasksService]
-// }
-).catch((err) => console.error(err));
+// }).catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent, {
+//     providers: [LoggingService]
+// }).catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
