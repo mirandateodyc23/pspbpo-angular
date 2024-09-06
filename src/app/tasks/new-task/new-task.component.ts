53b2942d47e8ccce95500149b5d44aeb3baab1d1
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-06 10:04:08 +0800

    using Custom DI Tokens & Provider

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, Inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
+import { TasksServiceToken } from '../../../main';
 
 @Component({
   selector: 'app-new-task',
@@ -14,7 +15,7 @@ export class NewTaskComponent {
 
   // private tasksService: TasksService;
 
-  constructor(private tService: TasksService) {
+  constructor(@Inject(TasksServiceToken) private tService: TasksService ) {
     // this.tasksService = new TasksService();
     // this.tasksService = tService;
   }
