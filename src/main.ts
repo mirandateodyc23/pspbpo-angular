192027591706efa9f5b03701aadd1b7e9a55ff75
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-09 16:42:07 +0800

    Zoneless

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+    providers: [provideExperimentalZonelessChangeDetection()]
+}).catch((err) => console.error(err));
