27b91d3f7722f16663eef0940624a640ee1e89a3
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-09 15:06:04 +0800

    inital project

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { TasksService } from './app/tasks/tasks.service';
-import { LoggingService } from './app/logging.service';
-import { InjectionToken } from '@angular/core';
 
-export const TasksServiceToken = new InjectionToken<TasksService>('tasks-service-token');
-
-bootstrapApplication(AppComponent, {
-    providers: [{provide: TasksServiceToken, useClass: TasksService }]
-}).catch((err) => console.error(err));
-
-// bootstrapApplication(AppComponent, {
-//     providers: [LoggingService]
-// }).catch((err) => console.error(err));
-
-// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
