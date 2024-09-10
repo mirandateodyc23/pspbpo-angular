6d698c916faadb7b047e436ab263a8d2ad55ba54
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-10 11:28:24 +0800

    signals vs observables - rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
-
-import { CounterComponent } from './counter/counter.component';
-import { MessagesComponent } from './messages/messages.component';
-
+import { Component, DestroyRef, effect, inject, OnInit, signal, computed } from '@angular/core';
+import { interval, map } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [CounterComponent, MessagesComponent],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  get debugOutput() {
-    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
-    return 'AppComponent Component Debug Output';
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+  interval = signal(0);
+  doubleInterval = computed(() => this.interval() * 2);
+
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.interval()} times.`)
+    });
+  }
+
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.interval.update(prevIntervalNumber => prevIntervalNumber + 1);
+    //   // update some signal
+    // }, 1000);
+
+    const subcription = interval(1000).pipe(
+      map((val) => val * 2)
+    ).subscribe({
+      next: (val) => console.log(val),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subcription.unsubscribe();
+    })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
-
-bootstrapApplication(AppComponent, {
-    providers: [provideExperimentalZonelessChangeDetection()]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
+  .catch((err) => console.error(err));
