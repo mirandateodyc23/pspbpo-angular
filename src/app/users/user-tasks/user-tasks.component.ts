4f1fdbf9249c908cc06dd980cacd56ed134f4076
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-13 10:19:33 +0800

    setup routes, appConfig, routerOutlet, routerLink, routeLinkActive & withComponentInputBinding

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Input, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  userId = input.required<string>(); // using signal
+  // @Input({required: true}) userId!: string;
+
+  private usersService = inject(UsersService);
+
+  userName = computed(
+    () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  );
+
+  // get userName() { // using getter
+  //   return this.usersService.users.find((u) => u.id === this.userId)?.name;
+  // }
+
+  // userName?: string;
+  // @Input()
+  // set userId(uid: string) { // using setter
+  //   this.userName = this.usersService.users.find((u) => u.id === uid)?.name;
+  // }
+  
+
+
+  
+}
