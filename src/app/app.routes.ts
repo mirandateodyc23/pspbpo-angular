d660f62b13348447ef5e6b59495edc26f6457d97
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-13 11:56:14 +0800

    separate children routes, redirectTo, pathMatch, notfound,

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,24 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '', // <your-domain>
         component: NoTaskComponent,
+        // redirectTo: '/users/u1',
+        // pathMatch: 'full'
     },
-    // {
-    //     path: 'tasks', // <your-domain>/tasks
-    //     component: TasksComponent,
-    // },
     {
         path: 'users/:userId', // <your-domain>/users/<uid>
         component: UserTasksComponent,
-        children: [
-            {
-                path: 'tasks', // <your-domain>/users/<uid>/tasks
-                component: TasksComponent
-            },
-            {
-                path: 'tasks/new',
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes
     },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+
 ];
