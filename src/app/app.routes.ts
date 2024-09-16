7169ee2b3fa9c64fe6c6037741b97e4ef54d5480
Teody Miranda <tmiranda@pacificseainvests.com>
2024-09-16 11:27:29 +0800

    RouteGuard: canMatch, canDeactivate, reload Resolver using navigate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,31 @@
-import { Routes } from '@angular/router';
+import { CanMatch, CanMatchFn, RedirectCommand, Route, Router, Routes, UrlSegment } from '@angular/router';
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UserNameResolver } from './users/user-name.resolver';
+import { inject, Injectable } from '@angular/core';
 
+@Injectable({ providedIn: 'root' })
+class CanMatchTeamSection implements CanMatch {
+  constructor(private router: Router) {}
+  canMatch(route: Route, segments: UrlSegment[]) {
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 1) {
+      return true;
+    }
+    return new RedirectCommand(this.router.parseUrl('/unauthorized'));
+  }
+}
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 0.5) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('./unauthorized'));
+}
 export const routes: Routes = [
     {
         path: '', // <your-domain>
@@ -17,6 +38,8 @@ export const routes: Routes = [
         path: 'users/:userId', // <your-domain>/users/<uid>
         component: UserTasksComponent,
         children: userRoutes,
+        // canMatch: [dummyCanMatch],
+        canMatch: [CanMatchTeamSection],
         data: {
             message: 'Hello!'
         },
--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
-import { NewTaskComponent } from '../tasks/new-task/new-task.component';
+import { canLeaveEditPage, NewTaskComponent } from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -12,7 +12,7 @@ export const routes: Routes = [
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
-    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
     },
@@ -20,5 +20,6 @@ export const routes: Routes = [
   {
     path: 'tasks/new',
     component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage]
   },
 ];
