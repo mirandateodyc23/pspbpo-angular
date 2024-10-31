de4255304b048ed59da166f4dc59254ab38377eb
Teody <tmiranda@pacificseainvests.com>
2024-10-31 16:05:30 +0800

    job title translation

--- src/app/main-content/jobs/jobs.component.ts
+++ src/app/main-content/jobs/jobs.component.ts
@@ -1,40 +1,89 @@
-import { Component, DestroyRef, inject, Input, input, OnInit } from '@angular/core';
-import { ContainerRowSectionComponent } from "../../shared/container-row-section/container-row-section.component";
+import {
+  Component,
+  DestroyRef,
+  inject,
+  Input,
+  input,
+  OnInit,
+} from '@angular/core';
+import { ContainerRowSectionComponent } from '../../shared/container-row-section/container-row-section.component';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [ContainerRowSectionComponent],
+  imports: [ContainerRowSectionComponent, TranslateModule, CommonModule],
   templateUrl: './jobs.component.html',
-  styleUrl: './jobs.component.css'
+  styleUrl: './jobs.component.css',
 })
-export class JobsComponent implements OnInit{
-  @Input() category: string = "";
-  @Input() position: string = "";
-  @Input() nationality: string = "";
+export class JobsComponent implements OnInit {
+  @Input() category: string = '';
+  @Input() position: string = '';
+  @Input() nationality: string = '';
 
   private destroyRef = inject(DestroyRef);
+  jobContent?: {
+    id: string;
+    dept_no: number;
+    dept: string;
+    title: string;
+    market: string;
+    slug: string;
+    responsibilities: [];
+    requirements: [];
+    other_requirements: [];
+    educational_background: [];
+  };
 
-  constructor(private route: ActivatedRoute){
-    console.log(this.category, this.position, this.nationality);
-  }
+  marketList: string[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private translate: TranslateService
+  ) {}
 
   ngOnInit() {
-      const subscription = this.route.params.subscribe(params => {
-        this.category = params['category'];
-        this.position = params['position'];
-        this.nationality = params['nationality'];
-        console.log(this.category, this.position, this.nationality);
-      });
-      this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    const subscription = this.route.params.subscribe((params) => {
+      this.category = params['category'];
+      this.position = params['position'];
+      this.nationality = params['nationality'];
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
 
-        // this.category = this.route.snapshot.params['category'];
-        // this.position = this.route.snapshot.params['position'];
-        // this.nationality = this.route.snapshot.params['nationality'];
-        // console.log(this.category, this.position, this.nationality);
-
+    this.getJobListDetails();
   }
 
+  getJobListDetails() {
+    this.translate.get('jobs.contents').subscribe((contents: {category: string, job_lists: [] }[]) => {
+      // Filter contents by category
+      const filteredContents = contents.filter((content) => content.category === this.category.toUpperCase());
 
+      // Check if there are any filtered contents
+      filteredContents.map(lists => {
+        const joblist = lists.job_lists;
+        joblist.filter(
+            (job: {
+              id: string;
+              dept_no: number;
+              dept: string;
+              title: string;
+              market: string;
+              slug: string;
+              responsibilities: [];
+              requirements: [];
+              other_requirements: [];
+              educational_background: [];
+            }) => job.slug === this.position
+          )
+          .map((job) => {
+            this.jobContent = job;
+          });
+      });
+
+      this.marketList =
+        this.jobContent?.market.split(',').map((m: string) => m.trim()) || [];
+    });
+  }
 }
